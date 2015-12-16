0f9949101d7692df7cf228e20d09c2e8e11b94d9
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-12-16 19:49:38 +0800

    Update copyright.

--- library/src/main/java/me/zhanghai/android/seekbarpreference/SeekBarPreference.java
+++ library/src/main/java/me/zhanghai/android/seekbarpreference/SeekBarPreference.java
@@ -1,7 +1,7 @@
 package me.zhanghai.android.seekbarpreference;
 
 /*
- * Copyright (c) 2014 Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
+ * Copyright (c) 2015 Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
  * All Rights Reserved.
  */
 
